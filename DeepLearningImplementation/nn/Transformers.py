705ff46d3ed0c62676ffdfac0d2066bea4f665e6
imraviagrawal <raviagrawal@umass.edu>
2023-05-30 20:44:40 -0700

    Transformers encoder and decoder layers

--- DeepLearningImplementation/nn/Transformers.py
+++ DeepLearningImplementation/nn/Transformers.py
@@ -19,32 +19,32 @@ pytorch.
 """
 import torch
 from torch import nn
-from modules import MultiheadAttention
+from modules import MultiHeadAttention
 
-class EncoderBlock(nn.Module):
-    def __init__(self, input_dim, num_heads, dim_feedforward, dropout=0.0):
-        """
-                Inputs:
-                    input_dim - Dimensionality of the input
-                    num_heads - Number of heads to use in the attention block
-                    dim_feedforward - Dimensionality of the hidden layer in the MLP
-                    dropout - Dropout probability to use in the dropout layers
-                """
+class TransformerEncoderLayer(nn.Module):
+    def __init__(
+        self,
+        dim_model: int = 512,
+        num_heads: int = 6,
+        dim_feedforward: int = 2048,
+        dropout: float = 0.1,
+    ):
         super().__init__()
+        dim_q = dim_k = max(dim_model // num_heads, 1)
 
         # Attention layer
-        self.self_attn = MultiheadAttention(input_dim, input_dim, num_heads)
-        self.linear_net = nn.Sequential(nn.Linear(input_dim, dim_feedforward),
+        self.self_attn = MultiHeadAttention(num_heads, dim_model, dim_q, dim_k)
+        self.linear_net = nn.Sequential(nn.Linear(dim_model, dim_feedforward),
                                         nn.Dropout(dropout),
                                         nn.ReLU(inplace=True),
-                                        nn.Linaer(dim_feedforward, input_dim))
-        self.norm1 = nn.LayerNorm(input_dim)
-        self.norm2 = nn.LayerNorm(input_dim)
+                                        nn.Linaer(dim_feedforward, dim_model))
+        self.norm1 = nn.LayerNorm(dim_model)
+        self.norm2 = nn.LayerNorm(dim_model)
         self.dropout = nn.Dropout(dropout)
 
-    def forward(self, x, mask=None):
-        attn_out = self.self_attn(x, mask=mask)
-        x = x + self.dropout(attn_out)
+    def forward(self, src, mask=None):
+        attn_out = self.self_attn(src, src, src)
+        x = src + self.dropout(attn_out)
         x = self.norm1(x)
 
         # mlp
@@ -54,10 +54,10 @@ class EncoderBlock(nn.Module):
 
         return x
 
-class Transformers(nn.Module):
+class TransformerEncoder(nn.Module):
     def __init__(self, num_layers, **block_args):
         super().__init__()
-        self.layers = nn.ModuleList([EncoderBlock(**block_args) for _ in range(num_layers)])
+        self.layers = nn.ModuleList([TransformerEncoderLayer(**block_args) for _ in range(num_layers)])
 
     def forward(self, x, mask=None):
         for l in self.layers:
@@ -71,3 +71,58 @@ class Transformers(nn.Module):
             attention_maps.append(attn_map)
             x = l(x)
         return attention_maps
+
+class TransformerDecoderLayer(nn.Module):
+    def __init__(self, dim_model: int = 512,
+        num_heads: int = 6,
+        dim_feedforward: int = 2048,
+        dropout: float = 0.1,
+    ):
+        super().__init__()
+        dim_q = dim_k = max(dim_model // num_heads, 1)
+        self.attn1 = MultiHeadAttention(num_heads, dim_model, dim_q, dim_q)
+        self.attn2 = MultiHeadAttention(num_heads, dim_model, dim_q, dim_k)
+
+        self.linear_net = nn.Sequential(nn.Linear(dim_model, dim_feedforward),
+                                        nn.Dropout(dropout),
+                                        nn.ReLU(inplace=True),
+                                        nn.Linear(dim_feedforward, dim_model)
+                                        )
+        self.dropout = nn.Dropout(dropout)
+        self.norm1 = nn.LayerNorm(dim_model)
+        self.norm2 = nn.LayerNorm(dim_model)
+        self.norm3 = nn.LayerNorm(dim_model)
+
+    def forward(self, src, tgt):
+        attn1 = self.attn1(src, src, src)
+        x = src + self.dropout(attn1)
+        x = self.norm1(x)
+
+        attn2 = self.attn2(src, tgt, tgt)
+        x = src + self.dropout(attn2)
+        x = self.norm2(x)
+
+        x = self.linear_net(x)
+        x = src + self.dropout(x)
+        x = self.norm3(x)
+        return x
+
+class TransformerDecoder(nn.Module):
+    def __init__(self,
+                 num_layers: int = 32,
+                 dim_model = 512,
+                 num_heads = 12,
+                 dim_feedforward = 2048,
+                 dropout: float = 0.1,
+                 ):
+        self.layers = nn.ModuleList([TransformerDecoderLayer(dim_model, num_heads, dim_feedforward, dropout) for _ in range(num_layers)])
+        self.linear = nn.Linear(dim_model, dim_feedforward)
+
+    def forward(self, src, tgt):
+        seq_len, dimension = src.size(1), src.size(2)
+        for layer in self.layers:
+            src = layer(src, tgt)
+        return torch.softmax(self.linear(src), dim=-1)
+
+
+
